4c4f46b65ed8ae46240e6ed0397dac8016d6ecc4
a-als <a_al-s@mail.com>
2025-02-02 04:06:12 +0300

    Add the correct pages for each Topic

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ pdf = FPDF(orientation='p', unit="mm", format="a4")
 
 df = pd.read_csv("topics.csv")
 for _, row in df.iterrows():
+    # Main Page
     pdf.add_page()
     pdf.set_font(family="Times", style='B', size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
     pdf.line(10, 23, 200, 23)
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
 
 
 pdf.output("output.pdf")
